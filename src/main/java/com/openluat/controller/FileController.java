a4cea419e885dd9b04cded76574084ca87875709
yanjunjie <yanjunjie@airm2m.com>
2020-06-10 14:24:56 +0800

    update

---
+++ src/main/java/com/openluat/controller/FileController.java
@@ -0,0 +1,37 @@
+package com.openluat.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@RestController
+public class FileController {
+
+    @PostMapping("/uploadFile")
+    @ResponseBody
+    public String handleFileUpload(@RequestParam("file") MultipartFile file) {
+        if (!file.isEmpty()) {
+            try {
+                BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(new File(file.getOriginalFilename())));
+                out.write(file.getBytes());
+                out.flush();
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "上传失败," + e.getMessage();
+            }
+
+            return "上传成功";
+
+        } else {
+            return "上传失败，因为文件是空的.";
+        }
+    }
+}
---
+++ src/main/java/com/openluat/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.openluat.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    /*@PostMapping(path = "/")
+    public int postTest(@RequestBody String data) {
+        return data.length();
+    }
+
+    @GetMapping(path = "/")
+    public String getPost() {
+        return "Jeremy SSLTest";
+    }*/
+}
