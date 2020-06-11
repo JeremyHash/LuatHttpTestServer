94bf5360e2c86fed33ff08955217a02f62962e08
yanjunjie <yanjunjie@airm2m.com>
2020-06-11 16:05:10 +0800

    update

--- src/main/java/com/openluat/Application.java
+++ src/main/java/com/openluat/Application.java
@@ -1,16 +1,11 @@
 package com.openluat;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("JeremyHttpTestServer Start!");
     }
-
 }
