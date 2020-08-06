217b46af3f26aaeb74e55014e545fc5a3d0ea8f5
yanjunjie <yanjunjie@airm2m.com>
2020-08-06 11:50:11 +0800

    add redirecttest

--- src/main/java/com/openluat/controller/GetController.java
+++ src/main/java/com/openluat/controller/GetController.java
@@ -1,9 +1,14 @@
 package com.openluat.controller;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 
 @RestController
 public class GetController {
@@ -12,4 +17,29 @@ public class GetController {
     public ResponseEntity<String> getTest() {
         return new ResponseEntity<>("LuatHttpTestServerGetTestOK", HttpStatus.OK);
     }
+
+    @GetMapping(path = "/redirect302/1")
+    public void redirectHandler1(HttpServletResponse response) throws IOException {
+        response.sendRedirect("http://www.openluat.com/Product/images/720dmod/1.png");
+    }
+
+    @GetMapping(path = "/redirect302")
+    public ResponseEntity<String> redirectHandler2(HttpServletResponse response) throws IOException {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        ArrayList<String> list = new ArrayList<>();
+        list.add("http://www.openluat.com/Product/images/720dmod/1.png");
+        httpHeaders.put("Location", list);
+        return new ResponseEntity<>(httpHeaders, HttpStatus.FOUND);
+    }
+
+    @GetMapping(path = "/redirect301")
+    public ResponseEntity<String> redirectHandler3(HttpServletResponse response) throws IOException {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        ArrayList<String> list = new ArrayList<>();
+        list.add("http://www.openluat.com/Product/images/720dmod/1.png");
+        httpHeaders.put("Location", list);
+        return new ResponseEntity<>(httpHeaders, HttpStatus.MOVED_PERMANENTLY);
+    }
+
+
 }
