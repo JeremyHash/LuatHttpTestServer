2a5c9a50375f8e4847f42966165a225b80207860
yanjunjie <yanjunjie@airm2m.com>
2020-08-13 11:30:00 +0800

    update

--- src/main/java/com/openluat/controller/BreakDownloadController.java
+++ src/main/java/com/openluat/controller/BreakDownloadController.java
@@ -2,9 +2,6 @@ package com.openluat.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/openluat/controller/GetController.java
+++ src/main/java/com/openluat/controller/GetController.java
@@ -18,28 +18,32 @@ public class GetController {
         return new ResponseEntity<>("LuatHttpTestServerGetTestOK", HttpStatus.OK);
     }
 
-    @GetMapping(path = "/redirect302/1")
-    public void redirectHandler1(HttpServletResponse response) throws IOException {
-        response.sendRedirect("http://www.openluat.com/Product/images/720dmod/1.png");
-    }
 
-    @GetMapping(path = "/redirect302")
-    public ResponseEntity<String> redirectHandler2(HttpServletResponse response) throws IOException {
+    @GetMapping(path = "/redirect301")
+    public ResponseEntity<String> redirectHandler1() {
         HttpHeaders httpHeaders = new HttpHeaders();
         ArrayList<String> list = new ArrayList<>();
         list.add("http://www.openluat.com/Product/images/720dmod/1.png");
+        //list.add("http://36.7.87.100:90");
+        //list.add("http://39.156.69.79");
+        //list.add("https://www.baidu.com");
+        httpHeaders.put("location", list);
+        return new ResponseEntity<>(httpHeaders, HttpStatus.MOVED_PERMANENTLY);
+    }
+
+    @GetMapping(path = "/redirect302")
+    public ResponseEntity<String> redirectHandler2() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        ArrayList<String> list = new ArrayList<>();
+        list.add("https://www.baidu.com");
         httpHeaders.put("Location", list);
         return new ResponseEntity<>(httpHeaders, HttpStatus.FOUND);
     }
 
-    @GetMapping(path = "/redirect301")
-    public ResponseEntity<String> redirectHandler3(HttpServletResponse response) throws IOException {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        ArrayList<String> list = new ArrayList<>();
-        list.add("http://www.openluat.com/Product/images/720dmod/1.png");
-        httpHeaders.put("Location", list);
-        return new ResponseEntity<>(httpHeaders, HttpStatus.MOVED_PERMANENTLY);
-    }
+    /*@GetMapping(path = "/redirect302")
+    public void redirectHandler3(HttpServletResponse response) throws IOException {
+        response.sendRedirect("http://www.openluat.com/Product/images/720dmod/1.png");
+    }*/
 
 
 }
